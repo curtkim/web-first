e73a96fff3ca717f3a3039206795b1e0c89d9c02
curt-kim <curt.kim@daumkakao.com>
2018-09-12 19:11:02 +0900

    kafka-stream windowedtest추가

--- kafka-stream/src/test/java/WindowedTest.java
+++ kafka-stream/src/test/java/WindowedTest.java
@@ -6,20 +6,34 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.*;
 import org.apache.kafka.streams.*;
-import org.apache.kafka.streams.processor.*;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.TimeWindows;
+import org.apache.kafka.streams.kstream.Windowed;
+import org.apache.kafka.streams.kstream.internals.WindowedDeserializer;
+import org.apache.kafka.streams.kstream.internals.WindowedSerializer;
+import org.apache.kafka.streams.processor.StreamPartitioner;
 import org.apache.kafka.test.TestUtils;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
-public class PartitionTimestampSyncTest {
+public class WindowedTest {
   @ClassRule
   public static final EmbeddedSingleNodeKafkaCluster CLUSTER = new EmbeddedSingleNodeKafkaCluster();
 
   private static final String inputTopic = "input-topic";
   private static final String outputTopic = "output-topic";
+
+  StringSerializer stringSerializer = new StringSerializer();
+  StringDeserializer stringDeserializer = new StringDeserializer();
+  WindowedSerializer<String> windowedSerializer = new WindowedSerializer<>(stringSerializer);
+  WindowedDeserializer<String> windowedDeserializer = new WindowedDeserializer<>(stringDeserializer);
+  Serde<Windowed<String>> windowedSerde = Serdes.serdeFrom(windowedSerializer,windowedDeserializer);
+
 
   @BeforeClass
   public static void startKafkaCluster() throws Exception {
@@ -31,13 +45,16 @@ public class PartitionTimestampSyncTest {
   public void shouldCountUsersPerRegion() throws Exception {
     // Step 1: Configure and start the processor topology.
 
-    Topology topology = new Topology();
-    StreamPartitioner<String, Integer> partitioner = (String k, Integer v, int numPartitions)-> 0;
-    topology.addSource("SOURCE", inputTopic)
-        .addProcessor("PROCESS", () -> new MyProcessor(), "SOURCE")
-        .addSink("SINK", outputTopic, new StringSerializer(), new IntegerSerializer(), partitioner, "PROCESS");
+    StreamsBuilder builder = new StreamsBuilder();
+    StreamPartitioner<Windowed<String>, Integer> partitioner = (Windowed<String> k, Integer v, int numPartitions)-> 0;
+    KStream<String, Integer> stream = builder.stream(inputTopic, Consumed.with(Serdes.String(), Serdes.Integer()));
+    stream.groupByKey()
+        .windowedBy(TimeWindows.of(TimeUnit.MINUTES.toMillis(1)))
+        .reduce((a,b)-> a+b)
+        .toStream()
+        .to(outputTopic, Produced.with(windowedSerde, Serdes.Integer(), partitioner));
 
-    KafkaStreams streams = new KafkaStreams(topology, getStreamProperties());
+    KafkaStreams streams = new KafkaStreams(builder.build(), getStreamProperties());
     streams.setUncaughtExceptionHandler((Thread thread, Throwable throwable) -> {
       System.err.println(throwable);
     });
@@ -47,32 +64,33 @@ public class PartitionTimestampSyncTest {
     // Step 2: Publish
     int MINUTE = 10;
     // 파티션0
-    List<ProducerRecord<String, String>> records = new ArrayList<>();
+    List<ProducerRecord<String, Integer>> records = new ArrayList<>();
     for(long l = 0; l < MINUTE*60; l++)
-      records.add(new ProducerRecord<>(inputTopic, 0, l*1000, "A", "A"));
-    IntegrationTestUtils2.produceRecordsSynchronously(records, getProducerProperties(), new StringSerializer(), new StringSerializer());
+      records.add(new ProducerRecord<>(inputTopic, 0, l*1000, "A", 1));
+    IntegrationTestUtils2.produceRecordsSynchronously(records, getProducerProperties(), new StringSerializer(), new IntegerSerializer());
 
     // 파티션1
-    records = new ArrayList<>();
+    List<ProducerRecord<String, Integer>> records2 = new ArrayList<>();
     for(long l = 0; l < MINUTE*60; l++)
-      records.add(new ProducerRecord<>(inputTopic, 1, l*1000, "B", "B"));
-    IntegrationTestUtils2.produceRecordsSynchronously(records, getProducerProperties(), new StringSerializer(), new StringSerializer());
+      records2.add(new ProducerRecord<>(inputTopic, 1, l*1000, "B", 2));
+    IntegrationTestUtils2.produceRecordsSynchronously(records2, getProducerProperties(), new StringSerializer(), new IntegerSerializer());
 
-    //
-    List<ConsumerRecord<String, Integer>> results = IntegrationTestUtils2.waitUntilMinKeyValueRecordsReceived(
+    // 파티션0
+    IntegrationTestUtils2.produceRecordsSynchronously(records, getProducerProperties(), new StringSerializer(), new IntegerSerializer());
+
+
+    // Step 3
+    List<ConsumerRecord<Windowed<String>, Integer>> results = IntegrationTestUtils2.waitUntilMinKeyValueRecordsReceived(
         getConsumerProperties(),
         outputTopic,
-        20,
-        new StringDeserializer(),
+        10,
+        windowedSerde.deserializer(),
         new IntegerDeserializer(),
         10*1000);
     streams.close();
 
-    // outputTopic에서 Record가 timestamp로 정렬되어 있기를 기대하지만, 실제로는 그렇지 않다.
-    // 1개의 Thread에서 2개의 task가 실행되고
-    // task는 서로 연관없이 병렬로 실행되기 때문에, timestamp가 꼬일 수 있다.
-    for(ConsumerRecord<String, Integer> rec : results)
-      System.out.println(rec);
+    for(ConsumerRecord<Windowed<String>, Integer> rec : results)
+      System.out.println(rec + " " + rec.key().window().start() + "~" + rec.key().window().end());
   }
 
   private Properties getStreamProperties() {
@@ -80,10 +98,14 @@ public class PartitionTimestampSyncTest {
     streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "test");
     streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, CLUSTER.bootstrapServers());
     streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-    streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+    streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Integer().getClass().getName());
     // The commit interval for flushing records to state stores and downstream must be lower than
     // this integration test's timeout (30 secs) to ensure we observe the expected processing results.
-    streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 10 * 1000);
+    //streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 10 * 1000);
+    //streamsConfiguration.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 10 * 1024 * 1024L);
+    streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 10*1000);
+    streamsConfiguration.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 10*1024*1024L);
+
     streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
     // Use a temporary directory for storing state, which will be automatically removed after the test.
     streamsConfiguration.put(StreamsConfig.STATE_DIR_CONFIG, TestUtils.tempDirectory().getAbsolutePath());
@@ -95,8 +117,6 @@ public class PartitionTimestampSyncTest {
     consumerConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, CLUSTER.bootstrapServers());
     consumerConfig.put(ConsumerConfig.GROUP_ID_CONFIG, "user-regions-lambda-integration-test-standard-consumer");
     consumerConfig.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-    consumerConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-    consumerConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class);
     return consumerConfig;
   }
 
@@ -105,50 +125,8 @@ public class PartitionTimestampSyncTest {
     userRegionsProducerConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, CLUSTER.bootstrapServers());
     userRegionsProducerConfig.put(ProducerConfig.ACKS_CONFIG, "all");
     userRegionsProducerConfig.put(ProducerConfig.RETRIES_CONFIG, 0);
-    userRegionsProducerConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-    userRegionsProducerConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
     return userRegionsProducerConfig;
   }
 
 }
 
-class MyProcessor implements Processor<String, String>{
-
-  ProcessorContext context;
-  Map<String, Integer> map = new HashMap<>();
-
-  @Override
-  public void init(ProcessorContext context) {
-    this.context = context;
-    this.context.schedule(60*1000, PunctuationType.STREAM_TIME, this::flush);
-  }
-
-  public void flush(long timestamp){
-    System.out.println(Thread.currentThread().getName() + " " + context.taskId() + " " + timestamp);
-    for(String key : map.keySet())
-      context.forward(key, map.get(key));
-
-    context.commit();
-    map = new HashMap<>();
-  }
-
-  @Override
-  public void process(String key, String value) {
-    Integer oldValue = map.get(value);
-    if (oldValue == null)
-      map.put(value, 1);
-    else
-      map.put(value, oldValue + 1);
-
-  }
-
-  @Override
-  public void punctuate(long timestamp) {
-
-  }
-
-  @Override
-  public void close() {
-
-  }
-}

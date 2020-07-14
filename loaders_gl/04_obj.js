1c4c48a7e8a769c9b1b499d4483b1ff815fa1eb6
curt.k <curt.k@kakaocorp.com>
2020-07-14 18:46:17 +0900

    loaders_lg에 obj파일 테스트

--- loaders_gl/04_obj.js
+++ loaders_gl/04_obj.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
-import {PCDLoader} from '@loaders.gl/pcd';
+import {OBJLoader} from '@loaders.gl/obj';
 import {parse} from '@loaders.gl/core';
 //import {registerLoaders} from '@loaders.gl/core';
 
 async function main(){
-    const data = await parse(fs.readFileSync('lidar.pcd'), PCDLoader, {onProgress: function(a){console.log(a.progress)}});
+    const data = await parse(fs.readFileSync('000001_points.obj'), OBJLoader, {onProgress: function(a){console.log(a.progress)}});
     console.log(data);
     console.log(data.header.boundingBox);
 }

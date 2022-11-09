d63fbee0bce25ac0ea85411cbb5d0350dd99e473
curt <curt.k@kakaomobility.com>
2022-11-09 18:36:12 +0900

    arrow

---
+++ arrow-first/src/main/java/WriteStreamArrowFile.java
@@ -0,0 +1,85 @@
+import org.apache.arrow.memory.BufferAllocator;
+import org.apache.arrow.memory.RootAllocator;
+import org.apache.arrow.vector.FieldVector;
+import org.apache.arrow.vector.IntVector;
+import org.apache.arrow.vector.VarCharVector;
+import org.apache.arrow.vector.VectorSchemaRoot;
+import org.apache.arrow.vector.dictionary.Dictionary;
+import org.apache.arrow.vector.dictionary.DictionaryEncoder;
+import org.apache.arrow.vector.dictionary.DictionaryProvider;
+import org.apache.arrow.vector.ipc.ArrowFileWriter;
+import org.apache.arrow.vector.ipc.ArrowStreamReader;
+import org.apache.arrow.vector.ipc.ArrowStreamWriter;
+import org.apache.arrow.vector.types.pojo.*;
+
+import java.io.*;
+import java.nio.channels.Channels;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Arrays.asList;
+
+public class WriteStreamArrowFile {
+
+
+  public static void main(String[] args) throws IOException {
+
+    BufferAllocator allocator = new RootAllocator();
+
+    // create dictionary and provider
+    final VarCharVector dictVector = new VarCharVector("dict", allocator);
+    dictVector.allocateNewSafe();
+    dictVector.setSafe(0, "aa".getBytes());
+    dictVector.setSafe(1, "bb".getBytes());
+    dictVector.setSafe(2, "cc".getBytes());
+    dictVector.setValueCount(3);
+
+    Dictionary dictionary = new Dictionary(dictVector, new DictionaryEncoding(1L, false, /*indexType=*/null));
+
+    // create vector and encode it
+    final VarCharVector vector = new VarCharVector("vector", allocator);
+    vector.allocateNewSafe();
+    vector.setSafe(0, "bb".getBytes());
+    vector.setSafe(1, "bb".getBytes());
+    vector.setSafe(2, "cc".getBytes());
+    vector.setSafe(3, "aa".getBytes());
+    vector.setValueCount(4);
+
+    // get the encoded vector
+    IntVector encodedVector = (IntVector) DictionaryEncoder.encode(vector, dictionary);
+
+    // create VectorSchemaRoot
+    List<Field> fields = Arrays.asList(encodedVector.getField());
+    List<FieldVector> vectors = Arrays.asList(encodedVector);
+    VectorSchemaRoot root = new VectorSchemaRoot(fields, vectors);
+
+    DictionaryProvider.MapDictionaryProvider provider = new DictionaryProvider.MapDictionaryProvider();
+    provider.put(dictionary);
+
+
+
+    // write data
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    {
+      ArrowStreamWriter writer = new ArrowStreamWriter(root, provider, Channels.newChannel(out));
+      writer.start();
+      writer.writeBatch();
+      writer.end();
+    }
+
+    // read data
+    try (ArrowStreamReader reader = new ArrowStreamReader(new ByteArrayInputStream(out.toByteArray()), allocator)) {
+      reader.loadNextBatch();
+      VectorSchemaRoot readRoot = reader.getVectorSchemaRoot();
+      // get the encoded vector
+      IntVector intVector = (IntVector) readRoot.getVector(0);
+
+      // get dictionaries and decode the vector
+      Map<Long, Dictionary> dictionaryMap = reader.getDictionaryVectors();
+      long dictionaryId = intVector.getField().getDictionary().getId();
+      VarCharVector varCharVector =(VarCharVector) DictionaryEncoder.decode(intVector, dictionaryMap.get(dictionaryId));
+    }
+  }
+}
---
+++ arrow-first/src/main/java/WriteStreamBatch.java
@@ -0,0 +1,76 @@
+import org.apache.arrow.memory.BufferAllocator;
+import org.apache.arrow.memory.RootAllocator;
+import org.apache.arrow.vector.*;
+import org.apache.arrow.vector.dictionary.Dictionary;
+import org.apache.arrow.vector.dictionary.DictionaryEncoder;
+import org.apache.arrow.vector.dictionary.DictionaryProvider;
+import org.apache.arrow.vector.ipc.ArrowStreamReader;
+import org.apache.arrow.vector.ipc.ArrowStreamWriter;
+import org.apache.arrow.vector.types.pojo.DictionaryEncoding;
+import org.apache.arrow.vector.types.pojo.Field;
+import org.apache.arrow.vector.types.pojo.Schema;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.channels.Channels;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class WriteStreamBatch {
+
+
+  public static void main(String[] args) throws IOException {
+
+    BufferAllocator allocator = new RootAllocator();
+
+    BitVector bitVector = new BitVector("boolean", allocator);
+    VarCharVector varCharVector = new VarCharVector("varchar", allocator);
+    for (int i = 0; i < 10; i++) {
+      bitVector.setSafe(i, i % 2 == 0 ? 0 : 1);
+      varCharVector.setSafe(i, ("test" + i).getBytes(StandardCharsets.UTF_8));
+    }
+    bitVector.setValueCount(10);
+    varCharVector.setValueCount(10);
+
+    List<Field> fields = Arrays.asList(bitVector.getField(), varCharVector.getField());
+    List<FieldVector> vectors = Arrays.asList(bitVector, varCharVector);
+    VectorSchemaRoot root = new VectorSchemaRoot(fields, vectors);
+
+
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    ArrowStreamWriter writer = new ArrowStreamWriter(root, /*DictionaryProvider=*/null, Channels.newChannel(out));
+
+
+    writer.start();
+    // write the first batch
+    writer.writeBatch();
+
+    // write another four batches.
+    for (int i = 0; i < 4; i++) {
+      // populate VectorSchemaRoot data and write the second batch
+      BitVector childVector1 = (BitVector) root.getVector(0);
+      VarCharVector childVector2 = (VarCharVector) root.getVector(1);
+      childVector1.reset();
+      childVector2.reset();
+      //... do some populate work here, could be different for each batch
+      writer.writeBatch();
+    }
+    // end
+    writer.end();
+
+
+    try (ArrowStreamReader reader = new ArrowStreamReader(new ByteArrayInputStream(out.toByteArray()), allocator)) {
+      Schema schema = reader.getVectorSchemaRoot().getSchema();
+      for (int i = 0; i < 5; i++) {
+        // This will be loaded with new values on every call to loadNextBatch
+        VectorSchemaRoot readBatch = reader.getVectorSchemaRoot();
+        reader.loadNextBatch();
+        //... do something with readBatch
+      }
+
+    }
+  }
+}

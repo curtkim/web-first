1d936f3ce0fe600d551e36a02de58732778c3f5b
curt <curt.k@kakaomobility.com>
2022-12-12 19:15:30 +0900

    arrow_duck추가

---
+++ arrow_duck/test-duck.ts
@@ -0,0 +1,36 @@
+//import { tableFromArrays } from 'apache-arrow';
+import { writeFileSync} from 'fs'
+
+import { tableFromIPC } from 'apache-arrow';
+import duckdb from 'duckdb'
+const db = new duckdb.Database(':memory:');
+const conn = db.connect();
+
+const query = "SELECT traffic_id, count(*) as cnt from read_parquet('*.parquet') group by traffic_id";
+
+conn.all(query, function (err, res){
+  if(err)
+    console.log(err)
+  else
+    console.log(res[0])
+});
+
+conn.exec("install arrow; load arrow;")
+
+conn.arrowIPCAll(query, function(err, res) {
+  if (err) {
+    throw err;
+  }
+
+  console.table([res]);
+  writeFileSync("temp.arrow", res)
+
+  const table = tableFromIPC(res);
+  console.log(table.schema)
+  console.table(table.toArray())
+
+  //console.log(typeof res) // object
+  //console.log(res.toString())
+  //console.log(res[0])
+});
+

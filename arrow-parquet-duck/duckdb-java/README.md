91917691d14beb346eda0a5ae7d9ae370748e07d
curt <curt.k@kakaomobility.com>
2023-01-15 14:14:14 +0900

    minor

--- arrow-parquet-duck/duckdb-java/README.md
+++ arrow-parquet-duck/duckdb-java/README.md
@@ -1,5 +1,9 @@
-## spring jdbc & duckdb
+## spring jdbc &duckdb
 
 ## test
 
     curl 'localhost:8080/list?day=20221204&trafficId=2280270100'
+
+## notes
+- duckdb를 jdbc로 사용할때 문제 없음
+- duckdb를 arrow로 가져올 때 문제
---
+++ arrow-parquet-duck/duckdb-java/src/test/java/com/example/duckdbfirst/JdbcArrowMain2.java
@@ -0,0 +1,20 @@
+package com.example.duckdbfirst;
+
+import java.sql.*;
+
+public class JdbcArrowMain2 {
+  public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    Class.forName("org.duckdb.DuckDBDriver");
+    Connection conn = DriverManager.getConnection("jdbc:duckdb:");
+//    Properties ro_prop = new Properties();
+//    ro_prop.setProperty("duckdb.read_only", "true");
+//    Connection conn_ro = DriverManager.getConnection("jdbc:duckdb:/tmp/my_database", ro_prop);
+
+    Statement stmt = conn.createStatement();
+    ResultSet rs = stmt.executeQuery("SELECT * FROM read_parquet('userdata1.parquet')");
+    while (rs.next()) {
+      System.out.println(String.format("%s %s %s", rs.getString(1), rs.getString(2), rs.getString(3)));
+    }
+    rs.close();
+  }
+}

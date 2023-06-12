69eaa60a56667ae0267cef7270bd69f9f5dce8c0
curt <curt.k@kakaomobility.com>
2023-06-12 15:20:28 +0900

    minor

--- arrow-parquet-duck/duckdb-java/src/test/java/com/example/duckdbfirst/SpatialExtentionMain.java
+++ arrow-parquet-duck/duckdb-java/src/test/java/com/example/duckdbfirst/SpatialExtentionMain.java
@@ -4,6 +4,7 @@ import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.io.InputStreamInStream;
 import org.locationtech.jts.io.ParseException;
 import org.locationtech.jts.io.WKBReader;
+import org.locationtech.jts.io.WKTReader;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -18,24 +19,53 @@ public class SpatialExtentionMain {
     prefare(conn);
     System.out.println("prefare " + (System.currentTimeMillis() - startTime)+ "ms");
 
-    WKBReader reader = new WKBReader();
-    Statement stmt = conn.createStatement();
-    String sql = "select linkid, rdcate, oneway, ST_AsText(ST_GeomFromWKB(wkb_geometry)) geom " +
-        "FROM st_read('/Users/curt/Documents/rn_link_l.fgb') " +
-        "limit 10";
-    ResultSet rs = stmt.executeQuery(sql);
-    System.out.println(rs.getMetaData().getColumnClassName(4));
-    System.out.println(rs.getMetaData().getColumnType(4));
-    System.out.println(rs.getMetaData().getColumnTypeName(4));
+    {
+      WKTReader reader = new WKTReader();
+      Statement stmt = conn.createStatement();
+      String sql = "select linkid, rdcate, oneway, ST_AsText(ST_GeomFromWKB(wkb_geometry)) geom " +
+          "FROM st_read('/Users/curt/Documents/rn_link_l.fgb') " +
+          "limit 10";
+      ResultSet rs = stmt.executeQuery(sql);
+      System.out.println("query " + (System.currentTimeMillis() - startTime) + "ms");
+      System.out.println(rs.getMetaData().getColumnClassName(4));
+      System.out.println(rs.getMetaData().getColumnType(4));
+      System.out.println(rs.getMetaData().getColumnTypeName(4));
 
-    System.out.println("query " + (System.currentTimeMillis() - startTime)+ "ms");
-    while (rs.next()) {
-      //ByteBuffer byteBuffer = (ByteBuffer)rs.getObject(4);
-      System.out.println(String.format("%s %s %s", rs.getString(1), rs.getString(2), rs.getString(3)));
-      String wkt = rs.getString(4);
-      System.out.println(wkt);
+      while (rs.next()) {
+        //ByteBuffer byteBuffer = (ByteBuffer)rs.getObject(4);
+        System.out.println(String.format("%s %s %s", rs.getString(1), rs.getString(2), rs.getString(3)));
+        String wkt = rs.getString(4);
+        Geometry geometry = reader.read(wkt);
+        System.out.println(geometry);
+      }
+      rs.close();
     }
-    rs.close();
+
+    {
+      WKBReader reader = new WKBReader();
+      Statement stmt = conn.createStatement();
+      String sql = "select linkid, rdcate, oneway, ST_AsWKB(ST_GeomFromWKB(wkb_geometry)) geom " +
+          "FROM st_read('/Users/curt/Documents/rn_link_l.fgb') " +
+          "limit 10";
+      ResultSet rs = stmt.executeQuery(sql);
+      System.out.println("query " + (System.currentTimeMillis() - startTime) + "ms");
+      System.out.println(rs.getMetaData().getColumnClassName(4));
+      System.out.println(rs.getMetaData().getColumnType(4));
+      System.out.println(rs.getMetaData().getColumnTypeName(4));
+
+      while (rs.next()) {
+        //ByteBuffer byteBuffer = (ByteBuffer)rs.getObject(4);
+        //System.out.println(String.format("%s %s %s", rs.getString(1), rs.getString(2), rs.getString(3)));
+        String a = rs.getString(1);
+        Blob blob = rs.getBlob(4);
+        /*
+        Blob blob = rs.getBlob(4);
+        Geometry geometry = reader.read(new InputStreamInStream(blob.getBinaryStream()));
+        System.out.println(geometry);
+         */
+      }
+      rs.close();
+    }
   }
 
   static void prefare(Connection conn) throws SQLException {
